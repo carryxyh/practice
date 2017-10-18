68d6ba779dc96ddd991a3aa2793870274102de8a
ziyuan <ziyuan@2dfire.com>
2017-10-18 21:39:55 +0800

    unsigned

--- main.cpp
+++ main.cpp
@@ -17,5 +17,14 @@ int main() {
     std::cout << i - u << std::endl; //这里把i变成无符号数 0
     std::cout << u - i << std::endl; //0
 
+    long double ld = 3.1415926536;
+    //这里会报错，因为可能丢失数据
+//    int a{ld}, b = {ld};
+//    std::cout << a << b << std::endl;
+
+    //相比上面，这里没有报错，但是确实丢失了数据
+    int c(ld), d = ld;
+    std::cout << c << d << std::endl;
+
     return 0;
 }
