f6d58aa8429a989866f28bda90a9f57fb2fed6e2
ziyuan <ziyuan@2dfire.com>
2017-10-18 21:19:54 +0800

    unsigned

--- main.cpp
+++ main.cpp
@@ -2,9 +2,20 @@
 
 int main() {
 
-    std::cout << "Enter two numbers:" << std::endl;
-    int v1 = 0, v2 = 0;
-    std::cin >> v1 >> v2;
-    std::cout << "sum is " << v1 + v2 << std::endl;
+//    std::cout << "Enter two numbers:" << std::endl;
+//    int v1 = 0, v2 = 0;
+//    std::cin >> v1 >> v2;
+//    std::cout << "sum is " << v1 + v2 << std::endl;
+
+    unsigned u = 10, u2 = 42;
+    std::cout << u2 - u << std::endl;//32
+    std::cout << u - u2 << std::endl;//这里必然是一个正数  4294967264
+
+    int i = 10, i2 = 42;
+    std::cout << i2 - i << std::endl; //32
+    std::cout << i - i2 << std::endl; //-32
+    std::cout << i - u << std::endl; //这里把i变成无符号数 0
+    std::cout << u - i << std::endl; //0
+
     return 0;
 }
