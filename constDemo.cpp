5aac47d4d06623d2e6d0f0576c164680a01a8deb
ziyuan <ziyuan@2dfire.com>
2017-10-21 15:44:05 +0800

    const

--- constDemo.cpp
+++ constDemo.cpp
@@ -2,11 +2,32 @@
 // Created by 修宇航 on 2017/10/20.
 //
 
+/**
+ * 引用的类型必须与其所引用对象的类型一致，但是又两个例外：
+ *  1.初始化常量引用时，允许用任意表达式作为初始值，只要该表达式的结果能转换成引用的类型即可
+ *
+ * @return
+ */
 int main() {
     const int ci = 1024;
     const int &r1 = ci; //正确：引用及其对应的对象都是常量
 
     r1 = 42;//错误：r1是常量引用
-    int &r2 = ci;  //错误：视图让一个非常量引用指向一个常量对象
+    int &r2 = ci;  //错误：试图让一个非常量引用指向一个常量对象
+
+    //1.demo
+    int i = 42;
+    const int &ri = i; //允许将const int&绑定到一个普通的int对象上
+    const int &ri2 = 42; //正确：ri2是一个常量引用
+    const int &ri3 = ri * 2; //正确：ri3是一个常量引用
+    int &ri4 = ri * 2; //错误：ri4是一个普通的非常量引用
+
+    double dd = 3.14;
+    const int &rd = dd;
+    //以上两句等价于：
+//    const int temp = dd; 由double生成一个临时的整型变量
+//    const int &rd = temp; 让rd绑定这个临时量
+
+
     return 0;
 }
