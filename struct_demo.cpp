2bdeeea6afca751dd03386ab14e722762ced4cd4
ziyuan <ziyuan@2dfire.com>
2017-10-27 21:17:23 +0800

    struct

--- struct_demo.cpp
+++ struct_demo.cpp
@@ -11,11 +11,14 @@ int main() {
         std::string bookNo;
         unsigned units_sold = 0;
         double revenue = 0.0;
-    } accum;
+    } data1;
 
     //等价于下一句。 第二种写法比较好
-    Sales_data accm;
+    Sales_data data2;
 
+    double price = 0;
+    std::cin >> data1.bookNo >> data1.units_sold >> price;
+    data1.revenue = data1.units_sold * price;
 
     return 0;
 }
