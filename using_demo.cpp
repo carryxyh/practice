db55df7d107eb2eb6cf1a2f7edd5462f1cce6da9
ziyuan <ziyuan@2dfire.com>
2017-11-01 10:16:59 +0800

    using

---
+++ using_demo.cpp
@@ -0,0 +1,16 @@
+//
+// Created by 修宇航 on 2017/11/1.
+//
+
+#include <iostream>
+
+using std::cin
+
+int main() {
+
+    int i;
+    cin >> i; //正确：cin和std::cin 含义相同
+//    count << i; //错误：没有生命using std::cout
+
+    return 0;
+}
